3db3604e9eb9b21055fb61baf801dc73ccda0841
PeiLe <91683680+05lpl@users.noreply.github.com>
2023-09-04 21:59:30 +0800

    Add files via upload

---
+++ option.py
@@ -0,0 +1,26 @@
+import os
+
+import argparse
+
+class options():    
+    def init(self):
+        with open(self.opt.head_path, "r") as f:
+            str = f.read()
+        with open(self.opt.save_path, "w") as f:
+            f.write(str)
+    def get_opt(self):
+        parser = argparse.ArgumentParser()
+        print("day=(sunday)")
+        day=input(int)
+        print("month=")
+        month=input(int)
+        
+        parser.add_argument("--day", type=int, default=day, help="The starting day of the week.")
+        parser.add_argument("--month", type=int, default=month, help="The starting day of the week.")
+        parser.add_argument("--year", type=int, default=2023, help="The starting day of the week.")
+        parser.add_argument("--save_path", type=str, default='export.ics', help="The path to store the calender.")
+        parser.add_argument("--read_path", type=str, default="date.txt", help="The read path.")
+        parser.add_argument("--head_path", type=str, default="header.txt", help="The first few lines of the document.")
+        self.opt = parser.parse_args()
+        self.init()
+        return self.opt
